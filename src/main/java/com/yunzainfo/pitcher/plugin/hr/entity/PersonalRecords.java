3699a3819916fdef29e5b4b1d45d815961d4491f
徐成 <old_camel@126.com>
2017-08-18 17:58:08 +0800

    Initial commit

---
+++ src/main/java/com/yunzainfo/pitcher/plugin/hr/entity/PersonalRecords.java
@@ -0,0 +1,1056 @@
+package com.yunzainfo.pitcher.plugin.hr.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PersonalRecords {
+    private String id;
+
+    private String deptId;
+
+    private String xm;
+
+    private BigDecimal sex;
+
+    private String sf;
+
+    private Date csrq;
+
+    private String mz;
+
+    private String jg;
+
+    private String zgxl;
+
+    private String xw;
+
+    private String qrzxl;
+
+    private Date bysjOne;
+
+    private String sxzyOne;
+
+    private String hqxl;
+
+    private Date hqxlsj;
+
+    private Date sxsj;
+
+    private String zzdz;
+
+    private String cym;
+
+    private String sfzhm;
+
+    private String byyxOne;
+
+    private String sxzyTwo;
+
+    private String byyxZz;
+
+    private String hqxw;
+
+    private Date hqxwsj;
+
+    private String hqxwyx;
+
+    private String hqxwzy;
+
+    private BigDecimal zzmm;
+
+    private String hshdrd;
+
+    private Date cjgzsj;
+
+    private Date lbdwsj;
+
+    private BigDecimal zzzt;
+
+    private Date txsj;
+
+    private Date dcsj;
+
+    private Date bgsj;
+
+    private Date swsj;
+
+    private Date lxsj;
+
+    private String szks;
+
+    private String yxzzw;
+
+    private String zj;
+
+    private Date rxzsj;
+
+    private Date rtzjsj;
+
+    private String zyjsxl;
+
+    private String jszw;
+
+    private BigDecimal przt;
+
+    private Date prsj;
+
+    private String yhzg;
+
+    private Date zgqdsj;
+
+    private String jxjb;
+
+    private Date sssj;
+
+    private BigDecimal jh;
+
+    private BigDecimal fd;
+
+    private BigDecimal xh;
+
+    private String dah;
+
+    private BigDecimal gjjszgz;
+
+    private BigDecimal dazdw;
+
+    private String grjl;
+
+    private String tc;
+
+    private String jtcy;
+
+    private String czdz;
+
+    private String czdzyb;
+
+    private String zzdzyb;
+
+    private String bgsdh;
+
+    private String zzdh;
+
+    private String chj;
+
+    private String yddh;
+
+    private String dzyx;
+
+    private String gzlx;
+
+    private String xzw;
+
+    private String jb;
+
+    private BigDecimal zwgz;
+
+    private String gzjb;
+
+    private Date gzzxsj;
+
+    private BigDecimal gzdc;
+
+    private String gze;
+
+    private Date jjsj;
+
+    private Date ltsj;
+
+    private Date bdsj;
+
+    private BigDecimal jsdjgz;
+
+    private BigDecimal gwgzdc;
+
+    private BigDecimal gwgze;
+
+    private Date jdsj;
+
+    private String gw;
+
+    private String gwdj;
+
+    private BigDecimal zjgwgze;
+
+    private BigDecimal xj;
+
+    private BigDecimal xjgze;
+
+    private BigDecimal jxjt;
+
+    private BigDecimal tgjt;
+
+    private BigDecimal bljt;
+
+    private BigDecimal zfbt;
+
+    private BigDecimal djqnf;
+
+    private String khjc;
+
+    private String ndkh;
+
+    private String shbxh;
+
+    private String shbxnr;
+
+    private BigDecimal jhsyzm;
+
+    private String pxz;
+
+    private String bz;
+
+    private Date zxsj;
+
+    private String bzTwo;
+
+    private BigDecimal yjxbz;
+
+    private BigDecimal xjxbz;
+
+    private Date zxsjOne;
+
+    private String lb;
+
+    private String ssjb;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getXm() {
+        return xm;
+    }
+
+    public void setXm(String xm) {
+        this.xm = xm;
+    }
+
+    public BigDecimal getSex() {
+        return sex;
+    }
+
+    public void setSex(BigDecimal sex) {
+        this.sex = sex;
+    }
+
+    public String getSf() {
+        return sf;
+    }
+
+    public void setSf(String sf) {
+        this.sf = sf;
+    }
+
+    public Date getCsrq() {
+        return csrq;
+    }
+
+    public void setCsrq(Date csrq) {
+        this.csrq = csrq;
+    }
+
+    public String getMz() {
+        return mz;
+    }
+
+    public void setMz(String mz) {
+        this.mz = mz;
+    }
+
+    public String getJg() {
+        return jg;
+    }
+
+    public void setJg(String jg) {
+        this.jg = jg;
+    }
+
+    public String getZgxl() {
+        return zgxl;
+    }
+
+    public void setZgxl(String zgxl) {
+        this.zgxl = zgxl;
+    }
+
+    public String getXw() {
+        return xw;
+    }
+
+    public void setXw(String xw) {
+        this.xw = xw;
+    }
+
+    public String getQrzxl() {
+        return qrzxl;
+    }
+
+    public void setQrzxl(String qrzxl) {
+        this.qrzxl = qrzxl;
+    }
+
+    public Date getBysjOne() {
+        return bysjOne;
+    }
+
+    public void setBysjOne(Date bysjOne) {
+        this.bysjOne = bysjOne;
+    }
+
+    public String getSxzyOne() {
+        return sxzyOne;
+    }
+
+    public void setSxzyOne(String sxzyOne) {
+        this.sxzyOne = sxzyOne;
+    }
+
+    public String getHqxl() {
+        return hqxl;
+    }
+
+    public void setHqxl(String hqxl) {
+        this.hqxl = hqxl;
+    }
+
+    public Date getHqxlsj() {
+        return hqxlsj;
+    }
+
+    public void setHqxlsj(Date hqxlsj) {
+        this.hqxlsj = hqxlsj;
+    }
+
+    public Date getSxsj() {
+        return sxsj;
+    }
+
+    public void setSxsj(Date sxsj) {
+        this.sxsj = sxsj;
+    }
+
+    public String getZzdz() {
+        return zzdz;
+    }
+
+    public void setZzdz(String zzdz) {
+        this.zzdz = zzdz;
+    }
+
+    public String getCym() {
+        return cym;
+    }
+
+    public void setCym(String cym) {
+        this.cym = cym;
+    }
+
+    public String getSfzhm() {
+        return sfzhm;
+    }
+
+    public void setSfzhm(String sfzhm) {
+        this.sfzhm = sfzhm;
+    }
+
+    public String getByyxOne() {
+        return byyxOne;
+    }
+
+    public void setByyxOne(String byyxOne) {
+        this.byyxOne = byyxOne;
+    }
+
+    public String getSxzyTwo() {
+        return sxzyTwo;
+    }
+
+    public void setSxzyTwo(String sxzyTwo) {
+        this.sxzyTwo = sxzyTwo;
+    }
+
+    public String getByyxZz() {
+        return byyxZz;
+    }
+
+    public void setByyxZz(String byyxZz) {
+        this.byyxZz = byyxZz;
+    }
+
+    public String getHqxw() {
+        return hqxw;
+    }
+
+    public void setHqxw(String hqxw) {
+        this.hqxw = hqxw;
+    }
+
+    public Date getHqxwsj() {
+        return hqxwsj;
+    }
+
+    public void setHqxwsj(Date hqxwsj) {
+        this.hqxwsj = hqxwsj;
+    }
+
+    public String getHqxwyx() {
+        return hqxwyx;
+    }
+
+    public void setHqxwyx(String hqxwyx) {
+        this.hqxwyx = hqxwyx;
+    }
+
+    public String getHqxwzy() {
+        return hqxwzy;
+    }
+
+    public void setHqxwzy(String hqxwzy) {
+        this.hqxwzy = hqxwzy;
+    }
+
+    public BigDecimal getZzmm() {
+        return zzmm;
+    }
+
+    public void setZzmm(BigDecimal zzmm) {
+        this.zzmm = zzmm;
+    }
+
+    public String getHshdrd() {
+        return hshdrd;
+    }
+
+    public void setHshdrd(String hshdrd) {
+        this.hshdrd = hshdrd;
+    }
+
+    public Date getCjgzsj() {
+        return cjgzsj;
+    }
+
+    public void setCjgzsj(Date cjgzsj) {
+        this.cjgzsj = cjgzsj;
+    }
+
+    public Date getLbdwsj() {
+        return lbdwsj;
+    }
+
+    public void setLbdwsj(Date lbdwsj) {
+        this.lbdwsj = lbdwsj;
+    }
+
+    public BigDecimal getZzzt() {
+        return zzzt;
+    }
+
+    public void setZzzt(BigDecimal zzzt) {
+        this.zzzt = zzzt;
+    }
+
+    public Date getTxsj() {
+        return txsj;
+    }
+
+    public void setTxsj(Date txsj) {
+        this.txsj = txsj;
+    }
+
+    public Date getDcsj() {
+        return dcsj;
+    }
+
+    public void setDcsj(Date dcsj) {
+        this.dcsj = dcsj;
+    }
+
+    public Date getBgsj() {
+        return bgsj;
+    }
+
+    public void setBgsj(Date bgsj) {
+        this.bgsj = bgsj;
+    }
+
+    public Date getSwsj() {
+        return swsj;
+    }
+
+    public void setSwsj(Date swsj) {
+        this.swsj = swsj;
+    }
+
+    public Date getLxsj() {
+        return lxsj;
+    }
+
+    public void setLxsj(Date lxsj) {
+        this.lxsj = lxsj;
+    }
+
+    public String getSzks() {
+        return szks;
+    }
+
+    public void setSzks(String szks) {
+        this.szks = szks;
+    }
+
+    public String getYxzzw() {
+        return yxzzw;
+    }
+
+    public void setYxzzw(String yxzzw) {
+        this.yxzzw = yxzzw;
+    }
+
+    public String getZj() {
+        return zj;
+    }
+
+    public void setZj(String zj) {
+        this.zj = zj;
+    }
+
+    public Date getRxzsj() {
+        return rxzsj;
+    }
+
+    public void setRxzsj(Date rxzsj) {
+        this.rxzsj = rxzsj;
+    }
+
+    public Date getRtzjsj() {
+        return rtzjsj;
+    }
+
+    public void setRtzjsj(Date rtzjsj) {
+        this.rtzjsj = rtzjsj;
+    }
+
+    public String getZyjsxl() {
+        return zyjsxl;
+    }
+
+    public void setZyjsxl(String zyjsxl) {
+        this.zyjsxl = zyjsxl;
+    }
+
+    public String getJszw() {
+        return jszw;
+    }
+
+    public void setJszw(String jszw) {
+        this.jszw = jszw;
+    }
+
+    public BigDecimal getPrzt() {
+        return przt;
+    }
+
+    public void setPrzt(BigDecimal przt) {
+        this.przt = przt;
+    }
+
+    public Date getPrsj() {
+        return prsj;
+    }
+
+    public void setPrsj(Date prsj) {
+        this.prsj = prsj;
+    }
+
+    public String getYhzg() {
+        return yhzg;
+    }
+
+    public void setYhzg(String yhzg) {
+        this.yhzg = yhzg;
+    }
+
+    public Date getZgqdsj() {
+        return zgqdsj;
+    }
+
+    public void setZgqdsj(Date zgqdsj) {
+        this.zgqdsj = zgqdsj;
+    }
+
+    public String getJxjb() {
+        return jxjb;
+    }
+
+    public void setJxjb(String jxjb) {
+        this.jxjb = jxjb;
+    }
+
+    public Date getSssj() {
+        return sssj;
+    }
+
+    public void setSssj(Date sssj) {
+        this.sssj = sssj;
+    }
+
+    public BigDecimal getJh() {
+        return jh;
+    }
+
+    public void setJh(BigDecimal jh) {
+        this.jh = jh;
+    }
+
+    public BigDecimal getFd() {
+        return fd;
+    }
+
+    public void setFd(BigDecimal fd) {
+        this.fd = fd;
+    }
+
+    public BigDecimal getXh() {
+        return xh;
+    }
+
+    public void setXh(BigDecimal xh) {
+        this.xh = xh;
+    }
+
+    public String getDah() {
+        return dah;
+    }
+
+    public void setDah(String dah) {
+        this.dah = dah;
+    }
+
+    public BigDecimal getGjjszgz() {
+        return gjjszgz;
+    }
+
+    public void setGjjszgz(BigDecimal gjjszgz) {
+        this.gjjszgz = gjjszgz;
+    }
+
+    public BigDecimal getDazdw() {
+        return dazdw;
+    }
+
+    public void setDazdw(BigDecimal dazdw) {
+        this.dazdw = dazdw;
+    }
+
+    public String getGrjl() {
+        return grjl;
+    }
+
+    public void setGrjl(String grjl) {
+        this.grjl = grjl;
+    }
+
+    public String getTc() {
+        return tc;
+    }
+
+    public void setTc(String tc) {
+        this.tc = tc;
+    }
+
+    public String getJtcy() {
+        return jtcy;
+    }
+
+    public void setJtcy(String jtcy) {
+        this.jtcy = jtcy;
+    }
+
+    public String getCzdz() {
+        return czdz;
+    }
+
+    public void setCzdz(String czdz) {
+        this.czdz = czdz;
+    }
+
+    public String getCzdzyb() {
+        return czdzyb;
+    }
+
+    public void setCzdzyb(String czdzyb) {
+        this.czdzyb = czdzyb;
+    }
+
+    public String getZzdzyb() {
+        return zzdzyb;
+    }
+
+    public void setZzdzyb(String zzdzyb) {
+        this.zzdzyb = zzdzyb;
+    }
+
+    public String getBgsdh() {
+        return bgsdh;
+    }
+
+    public void setBgsdh(String bgsdh) {
+        this.bgsdh = bgsdh;
+    }
+
+    public String getZzdh() {
+        return zzdh;
+    }
+
+    public void setZzdh(String zzdh) {
+        this.zzdh = zzdh;
+    }
+
+    public String getChj() {
+        return chj;
+    }
+
+    public void setChj(String chj) {
+        this.chj = chj;
+    }
+
+    public String getYddh() {
+        return yddh;
+    }
+
+    public void setYddh(String yddh) {
+        this.yddh = yddh;
+    }
+
+    public String getDzyx() {
+        return dzyx;
+    }
+
+    public void setDzyx(String dzyx) {
+        this.dzyx = dzyx;
+    }
+
+    public String getGzlx() {
+        return gzlx;
+    }
+
+    public void setGzlx(String gzlx) {
+        this.gzlx = gzlx;
+    }
+
+    public String getXzw() {
+        return xzw;
+    }
+
+    public void setXzw(String xzw) {
+        this.xzw = xzw;
+    }
+
+    public String getJb() {
+        return jb;
+    }
+
+    public void setJb(String jb) {
+        this.jb = jb;
+    }
+
+    public BigDecimal getZwgz() {
+        return zwgz;
+    }
+
+    public void setZwgz(BigDecimal zwgz) {
+        this.zwgz = zwgz;
+    }
+
+    public String getGzjb() {
+        return gzjb;
+    }
+
+    public void setGzjb(String gzjb) {
+        this.gzjb = gzjb;
+    }
+
+    public Date getGzzxsj() {
+        return gzzxsj;
+    }
+
+    public void setGzzxsj(Date gzzxsj) {
+        this.gzzxsj = gzzxsj;
+    }
+
+    public BigDecimal getGzdc() {
+        return gzdc;
+    }
+
+    public void setGzdc(BigDecimal gzdc) {
+        this.gzdc = gzdc;
+    }
+
+    public String getGze() {
+        return gze;
+    }
+
+    public void setGze(String gze) {
+        this.gze = gze;
+    }
+
+    public Date getJjsj() {
+        return jjsj;
+    }
+
+    public void setJjsj(Date jjsj) {
+        this.jjsj = jjsj;
+    }
+
+    public Date getLtsj() {
+        return ltsj;
+    }
+
+    public void setLtsj(Date ltsj) {
+        this.ltsj = ltsj;
+    }
+
+    public Date getBdsj() {
+        return bdsj;
+    }
+
+    public void setBdsj(Date bdsj) {
+        this.bdsj = bdsj;
+    }
+
+    public BigDecimal getJsdjgz() {
+        return jsdjgz;
+    }
+
+    public void setJsdjgz(BigDecimal jsdjgz) {
+        this.jsdjgz = jsdjgz;
+    }
+
+    public BigDecimal getGwgzdc() {
+        return gwgzdc;
+    }
+
+    public void setGwgzdc(BigDecimal gwgzdc) {
+        this.gwgzdc = gwgzdc;
+    }
+
+    public BigDecimal getGwgze() {
+        return gwgze;
+    }
+
+    public void setGwgze(BigDecimal gwgze) {
+        this.gwgze = gwgze;
+    }
+
+    public Date getJdsj() {
+        return jdsj;
+    }
+
+    public void setJdsj(Date jdsj) {
+        this.jdsj = jdsj;
+    }
+
+    public String getGw() {
+        return gw;
+    }
+
+    public void setGw(String gw) {
+        this.gw = gw;
+    }
+
+    public String getGwdj() {
+        return gwdj;
+    }
+
+    public void setGwdj(String gwdj) {
+        this.gwdj = gwdj;
+    }
+
+    public BigDecimal getZjgwgze() {
+        return zjgwgze;
+    }
+
+    public void setZjgwgze(BigDecimal zjgwgze) {
+        this.zjgwgze = zjgwgze;
+    }
+
+    public BigDecimal getXj() {
+        return xj;
+    }
+
+    public void setXj(BigDecimal xj) {
+        this.xj = xj;
+    }
+
+    public BigDecimal getXjgze() {
+        return xjgze;
+    }
+
+    public void setXjgze(BigDecimal xjgze) {
+        this.xjgze = xjgze;
+    }
+
+    public BigDecimal getJxjt() {
+        return jxjt;
+    }
+
+    public void setJxjt(BigDecimal jxjt) {
+        this.jxjt = jxjt;
+    }
+
+    public BigDecimal getTgjt() {
+        return tgjt;
+    }
+
+    public void setTgjt(BigDecimal tgjt) {
+        this.tgjt = tgjt;
+    }
+
+    public BigDecimal getBljt() {
+        return bljt;
+    }
+
+    public void setBljt(BigDecimal bljt) {
+        this.bljt = bljt;
+    }
+
+    public BigDecimal getZfbt() {
+        return zfbt;
+    }
+
+    public void setZfbt(BigDecimal zfbt) {
+        this.zfbt = zfbt;
+    }
+
+    public BigDecimal getDjqnf() {
+        return djqnf;
+    }
+
+    public void setDjqnf(BigDecimal djqnf) {
+        this.djqnf = djqnf;
+    }
+
+    public String getKhjc() {
+        return khjc;
+    }
+
+    public void setKhjc(String khjc) {
+        this.khjc = khjc;
+    }
+
+    public String getNdkh() {
+        return ndkh;
+    }
+
+    public void setNdkh(String ndkh) {
+        this.ndkh = ndkh;
+    }
+
+    public String getShbxh() {
+        return shbxh;
+    }
+
+    public void setShbxh(String shbxh) {
+        this.shbxh = shbxh;
+    }
+
+    public String getShbxnr() {
+        return shbxnr;
+    }
+
+    public void setShbxnr(String shbxnr) {
+        this.shbxnr = shbxnr;
+    }
+
+    public BigDecimal getJhsyzm() {
+        return jhsyzm;
+    }
+
+    public void setJhsyzm(BigDecimal jhsyzm) {
+        this.jhsyzm = jhsyzm;
+    }
+
+    public String getPxz() {
+        return pxz;
+    }
+
+    public void setPxz(String pxz) {
+        this.pxz = pxz;
+    }
+
+    public String getBz() {
+        return bz;
+    }
+
+    public void setBz(String bz) {
+        this.bz = bz;
+    }
+
+    public Date getZxsj() {
+        return zxsj;
+    }
+
+    public void setZxsj(Date zxsj) {
+        this.zxsj = zxsj;
+    }
+
+    public String getBzTwo() {
+        return bzTwo;
+    }
+
+    public void setBzTwo(String bzTwo) {
+        this.bzTwo = bzTwo;
+    }
+
+    public BigDecimal getYjxbz() {
+        return yjxbz;
+    }
+
+    public void setYjxbz(BigDecimal yjxbz) {
+        this.yjxbz = yjxbz;
+    }
+
+    public BigDecimal getXjxbz() {
+        return xjxbz;
+    }
+
+    public void setXjxbz(BigDecimal xjxbz) {
+        this.xjxbz = xjxbz;
+    }
+
+    public Date getZxsjOne() {
+        return zxsjOne;
+    }
+
+    public void setZxsjOne(Date zxsjOne) {
+        this.zxsjOne = zxsjOne;
+    }
+
+    public String getLb() {
+        return lb;
+    }
+
+    public void setLb(String lb) {
+        this.lb = lb;
+    }
+
+    public String getSsjb() {
+        return ssjb;
+    }
+
+    public void setSsjb(String ssjb) {
+        this.ssjb = ssjb;
+    }
+}
